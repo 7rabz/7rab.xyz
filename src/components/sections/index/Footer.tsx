22b5b88d9c24becfa076fd2c7f5bd57b1a0aefb5
7rabz <157163341+7rabz@users.noreply.github.com>
2025-06-18 13:31:53 +0300

    Update Footer.tsx

--- src/components/sections/index/Footer.tsx
+++ src/components/sections/index/Footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
             <footer className="max-w-4xl w-full flex flex-col mx-auto pt-4 pb-2">
                 <Divider />
                 <p className="text-center font-semibold text-base pt-2">
-                    Aiden.gg - Made with NextJS, TailwindCSS, and ❤ by me
+                    7rab.xyz - Made with NextJS, TailwindCSS, and ❤ by me
                 </p>
                 <p className="text-center font-medium brightness-75 text-base">
-                    Last updated: June 10th, 2025
+                    Last updated: June 18th, 2025
                 </p>
             </footer>
         </>
