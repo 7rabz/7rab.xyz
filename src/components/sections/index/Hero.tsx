47ece5df4ec9ea28b028067bf7f4acc78ba28e76
7rabz <157163341+7rabz@users.noreply.github.com>
2025-07-23 13:48:37 +0300

    Update Hero.tsx

--- src/components/sections/index/Hero.tsx
+++ src/components/sections/index/Hero.tsx
@@ -23,7 +23,7 @@ export default function Hero({ inView, descRef }: { inView: boolean, descRef: an
               <Typewriter
                 options={{
                   delay: 30,
-                  strings: ["I'm a 17yo silly developer, living in Riyadh, Saudi Arabia"],
+                  strings: ["Just a very silly developer, looking for other silly friends! :D"],
                   loop: false,
                   autoStart: true,
                   deleteSpeed: 604800000,
