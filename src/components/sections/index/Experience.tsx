3bf91dd10f2ed1766ac8e949041d0c1e2103904b
sillyhexa <157163341+7rabz@users.noreply.github.com>
2025-09-01 11:04:57 +0300

    Update Experience.tsx

--- src/components/sections/index/Experience.tsx
+++ src/components/sections/index/Experience.tsx
@@ -19,25 +19,18 @@ export default function Experience() {
                 </motion.h1>
                 <ul className={`flex flex-col pt-6 pb-1 gap-4 overflow-hidden`}>
                     <ExperienceCard
-                        url="https://api.7rab.xyz/"
-                        title="Nightmare.cc"
+                        url="https://7rab.xyz/"
+                        title="7rab.xyz"
                         fullDescription={[
-                            "Nightmare is a Advanced Roblox Alt Controler, an Alt Control Script (ARAC, or ACS) | Nightmare can be used to manage your roblox workspace or just used for fun.",
-                            "I have been running Nightmare for over a year now, crazy right? Nightmare taught me alot of things that a basic programmer should know, of course one of them was security because we nearly got breached in the early months.",
-                            "Nightmare's most notable project is NightAuth, which helps people to integrate all their data into the script, kinda like a authentication system but tweaked. NightAuth is made with JS and hosted on Namecheap's Stellar subscription",
-                            "Nightmare is basically handled by a single developer, which is me, I host all projects and sub-projects for nightmare, and I handle all Backend + Frontend, along with User Data",
-                            "Nightmare was orignally based on Fermium, a fake \"Alt Controller\" which people bought and believed in, Fermium was actually already deployed & working, but it was just a popular roblox script called Nameless Admin, they couldnt have more than 1+ customer using the bot at once.",
-                            "But in the end, Nightmare is offically discontinued on 6/16/2025, I sadly can't handle the website nor script anymore, None of the sources are for sale! This has been a nice ride for all of us. Thank you <3"
+                            "meeeowwww :3"
                         ]}
-                        cardImage="https://cdn.discordapp.com/attachments/1304046095361507358/1409956415740838098/Nightmare.jpg?ex=68b683fd&is=68b5327d&hm=e4add5083f879d2507a95030dc0aae5d23bb5f813056784c2624a8881793d613&"
-                        cardDescription="Nightmare is a Advanced Roblox Alt Controler, an Alt Control Script (ARAC, or ACS) | Nightmare can be used to manage your roblox workspace or just used for fun."
+                        cardImage="https://7rab.xyz/me.png"
+                        cardDescription="very silly autobiography about myself 🤯"
                         media={[
-                            "https://files.catbox.moe/edplr6.mp4",
-                            "https://files.catbox.moe/u0hzzl.mp4",
-                            "https://files.catbox.moe/d6mtbp.mp4",
+                            "https://7rab.xyz/image.png",
                         ]}
-                        myRole="Founder/Head Developer"
-                        timeline="June 2024 - June 2025"
+                        myRole="Developer"
+                        timeline="June 16 - Current"
                         delay={0.1}
                         gradient="bg-gradient-to-br"
                     />
