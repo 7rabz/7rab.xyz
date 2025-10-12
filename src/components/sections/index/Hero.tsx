12fa5f8c5005a9f07cd16d2366ec2326e12d5593
sillyhexa <157163341+7rabz@users.noreply.github.com>
2025-10-12 11:36:40 +0300

    Update Hero.tsx

--- src/components/sections/index/Hero.tsx
+++ src/components/sections/index/Hero.tsx
@@ -23,7 +23,7 @@ export default function Hero({ inView, descRef }: { inView: boolean, descRef: an
               <Typewriter
                 options={{
                   delay: 30,
-                  strings: ["a very retarded developer, looking for other retarded developers duh obviously"],
+                  strings: ["a truly retarded developer, looking for other retarded developers duh obviously"],
                   loop: false,
                   autoStart: true,
                   deleteSpeed: 604800000,
