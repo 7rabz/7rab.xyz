43587e29967673238ddaea169e59dda8cdf52ef4
sillyhexa <157163341+7rabz@users.noreply.github.com>
2025-10-12 11:37:34 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,18 +9,18 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextSeo
-        title={"[ sillyhexa's page / bio ]"}
-        description={"mrawr :3 | Read more about me, just click the link above!"}
+        title={"[ type's page / bio ]"}
+        description={"mrrp | Read more about me, just click the link above!"}
         canonical={`https://7rab.xyz${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`}
         themeColor={"#2563eb"}
         openGraph={{
           url: `https://7rab.xyz${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`,
-          title: "[ sillyhexa's page / bio ]",
-          description: "mrawr :3 | Read more about me, just click the link above!",
+          title: "[ type's page / bio ]",
+          description: "mrrp | Read more about me, just click the link above!",
           images: [
             {
               url: "https://cdn.7rab.xyz/image.png",
-              alt: "silly hexa!",
+              alt: "type!",
             },
           ],
         }}
