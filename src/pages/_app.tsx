7cbf2df0226c43e38916a61a38bf7e05048ef989
! cat sitting in glass cup <157163341+7rabz@users.noreply.github.com>
2025-12-01 02:26:09 +0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,18 +9,18 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NextSeo
-        title={"[ type's page / bio ]"}
+        title={"[ hex's page / bio ]"}
         description={"mrrp | Read more about me, just click the link above!"}
         canonical={`https://7rab.xyz${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`}
         themeColor={"#2563eb"}
         openGraph={{
           url: `https://7rab.xyz${router.asPath.split("?")[0] === "/" ? "" : router.asPath.split("?")[0]}`,
-          title: "[ type's page / bio ]",
+          title: "[ hex's page / bio ]",
           description: "mrrp | Read more about me, just click the link above!",
           images: [
             {
               url: "https://cdn.7rab.xyz/image.png",
-              alt: "type!",
+              alt: "hex!",
             },
           ],
         }}
